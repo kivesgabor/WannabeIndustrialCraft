6d7c19d2222aa47cd2151f0dd5be7dc82204d017
kivesgabor <kivesgabor1991@gmail.com>
2021-02-09 14:14:26 +0100

    registering blocks and items

--- src/main/java/com/navarone/wannabeindustrialcraft/WICraft.java
+++ src/main/java/com/navarone/wannabeindustrialcraft/WICraft.java
@@ -1,5 +1,6 @@
 package com.navarone.wannabeindustrialcraft;
 
+import com.navarone.wannabeindustrialcraft.setup.Registration;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -19,13 +20,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("wannabeindustrialcraft")
+@Mod(WICraft.MOD_ID)
 public class WICraft
 {
+    public static final String MOD_ID = "wannabeindustrialcraft";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public WICraft() {
+        Registration.register();
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
--- src/main/java/com/navarone/wannabeindustrialcraft/setup/Registration.java
+++ src/main/java/com/navarone/wannabeindustrialcraft/setup/Registration.java
@@ -1,5 +1,23 @@
 package com.navarone.wannabeindustrialcraft.setup;
 
-public class Registration {
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import com.navarone.wannabeindustrialcraft.WICraft;
 
+public class Registration {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, WICraft.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WICraft.MOD_ID);
+
+    public static void register() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+
+        ModBlocks.register();
+        ModItems.register();
+    }
 }
